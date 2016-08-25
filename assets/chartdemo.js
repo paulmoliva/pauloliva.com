1616d8034c999dbbb77f12b0811aa6052facf553
paulmoliva <paulmoliva@gmail.com>
2016-08-25 13:31:39 -0700

    started charts demo

---
+++ assets/chartdemo.js
@@ -0,0 +1,40 @@
+function displayGeorgia() {
+  console.log('hi');
+  clearPoll();
+  presidentialPoll('georgia');
+  pollChart('georgia');
+}
+function displayNational() {
+  console.log('hi');
+  clearPoll();
+  presidentialPoll('national');
+  pollChart('national');
+}
+function display4WayNational() {
+  console.log('hi');
+  clearPoll();
+  presidentialPoll('four-way-national');
+  pollChart('four-way-national');
+}
+function displayGeorgia() {
+  console.log('hi');
+  clearPoll();
+  presidentialPoll('georgia');
+  pollChart('georgia');
+}
+function displayGeorgia() {
+  console.log('hi');
+  clearPoll();
+  presidentialPoll('georgia');
+  pollChart('georgia');
+}
+function displayGeorgia() {
+  console.log('hi');
+  clearPoll();
+  presidentialPoll('georgia');
+  pollChart('georgia');
+}
+
+function clearPoll() {
+  $('#polls-root').empty();
+}
