db355ae9edc9f341e7a64b98640512028849ac38
Paul Oliva <paulmoliva@gmail.come>
2016-12-04 18:15:27 -0800

    testing web service

---
+++ service.php
@@ -0,0 +1,17 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$resultArray() = array()
+array_push($resultArray, $server);
+array_push($resultArray, $username);
+array_push($resultArray, $password);
+array_push($resultArray, $db);
+echo json_encode($resultArray);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
